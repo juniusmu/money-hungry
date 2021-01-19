68f622d6f07b354d3d5a15e570465b86601818ca
Junius Murphy <juniusmu@umich.edu>
2021-01-18 20:51:43 -0600

    Move game instructions  display logic away from domain logic

--- app/display/static.swift
+++ app/display/static.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 
-func printLogo(){
+func displayGameLogo(){
     print(" _____                    _____                     ")
     print("|     |___ ___ ___ _ _   |  |  |_ _ ___ ___ ___ _ _ ")
     print("| | | | . |   | -_| | |  |     | | |   | . |  _| | |")
     print("|_|_|_|___|_|_|___|_  |  |__|__|___|_|_|_  |_| |_  |")
     print("                  |___|                |___|   |___|")
+}
+
+func displayGameInstructions(){
+    print(String(repeating: "\n", count: 100))
+    print("This is a game where you are a snake.")
+    print("Your goal is to avoid all of the obstacles.")
+    print("And rack up as much money as you can.")
+    print(" 'W' 'A' 'S' 'D' are the direction keys.")
+    print("Once you type in your direction, press the enter key to execute it")
+    print("If you take to long to move, you will die.")
+    print("press ENTER to start game.")
 }
--- app/main.swift
+++ app/main.swift
@@ -267,7 +267,7 @@ class Game {
         print(String(repeating: "\n", count: 22))
         isGameOver()
         if(firstLoad){
-            printLogo()
+            displayGameLogo()
             firstLoad = false
         }
         print("+" + String(repeating: "-", count: MAPWIDTH) + "+")
@@ -349,19 +349,8 @@ class Game {
     }
 }
 
-func tutorial(){
-     print(String(repeating: "\n", count: 100))
-        print("This is a game where you are a snake.")
-        print("Your goal is to avoid all of the obstacles.")
-        print("And rack up as much money as you can.")
-        print(" 'W' 'A' 'S' 'D' are the direction keys.")
-        print("Once you type in your direction, press the enter key to execute it")
-        print("If you take to long to move, you will die.")
-        print("press ENTER to start game.")
-        userInput = readLine() ?? ""
-}
-
 func startGame() {
+    userInput = readLine() ?? ""
     let game = Game()
     game.drawMap()
 
@@ -456,5 +445,5 @@ func startGame() {
     }
 }
 
-tutorial()
+displayGameInstructions()
 startGame()
